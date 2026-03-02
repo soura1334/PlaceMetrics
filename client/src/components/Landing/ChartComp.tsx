e8a332afd4ead86aec98edf9c885a0c8224d99d8
soura1334 <151785439+soura1334@users.noreply.github.com>
2026-03-02 18:17:32 +0530

    Added Logo and changed graph colors

--- client/src/components/Landing/ChartComp.tsx
+++ client/src/components/Landing/ChartComp.tsx
@@ -27,7 +27,7 @@ export default function ChartComp() {
             </linearGradient>
             <linearGradient id="areaGradient" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#3b82f6" stopOpacity={1} />
-              <stop offset="95%" stopColor="#3b82f6" stopOpacity={0} />
+              <stop offset="95%" stopColor="#14b8a6" stopOpacity={0.3} />
             </linearGradient>
           </defs>
           <Area
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
+import { TrendingUp } from "lucide-react";
+
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-20 py-2 bg-black/30 backdrop-blur-md border-b border-white/5 lg:text-md text-sm">
-      <div className="flex justify-between px-6 py-4 mx-auto max-w-7xl">
-        <div >PlaceMetrics</div>
+      <div className="flex items-center justify-between px-6 py-4 mx-auto max-w-7xl">
+        <div className="flex items-center gap-4"><TrendingUp className="h-5 lg:h-10" />PlaceMetrics</div>
         <ul className="flex justify-end gap-10">
           <li>Login</li>
           <li>Get Started</li>
